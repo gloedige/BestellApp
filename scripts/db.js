842a210636a37e452e48fc4672e3f5d62a881856
gloedige <gerd.loedige@posteo.de>
2025-09-04 14:13:18 +0200

    create dish db

---
+++ scripts/db.js
@@ -0,0 +1,34 @@
+let allDishes=[
+    {
+        "name": "Gebackenes Fischfilet nach Kim Kim Art (scharf)",
+        "description": "mit Gemüse und Knoblauch",
+        "price": 11.50,
+        "quantity": 0
+    },
+    {
+        "name": "Gebratene Nudeln (Tofu)",
+        "description": "mit Tofu und Gemüse",
+        "price": 10.00,
+        "quantity": 0
+    },
+     {
+        "name": "Gebackene Wan-Tan (5 Stück)",
+        "description": "mit Hühnerfleisch",
+        "price": 4.50,
+        "quantity": 0
+    },
+     {
+        "name": "Hühnerfleisch Saté (4 Stück)",
+        "description": "mit Erdnussauce",
+        "price": 10.00,
+        "quantity": 0
+    },
+     {
+        "name": "Bun Nem Ran",
+        "description": "Reisnudeln mit gebackenen Vietnamrollen, Salat und vietnamesischer Sauce",
+        "price": 12.50,
+        "quantity": 0
+    }
+]
+
+
