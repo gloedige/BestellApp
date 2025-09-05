c2f2f46b9949253a9615d114780f87d6e8f73d08
gloedige <gerd.loedige@posteo.de>
2025-09-05 11:05:01 +0200

    create function renderSingleDishSection

--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,15 @@
-function localStorageIsCleared() {
-  if (localStorage.length!=0){
-    return false; 
-  }
-  else{
-    return true;
-  }
-}
+function renderSingleDishSection(dishId, singleDishObj){
+  return `
+          <div class="single_dish" onmouseover="highlightDish('${dishId}')" onmouseout="highlightDish('${dishId}')" id="${dishId}">
+            <div class="dish_attributes">
+                <h4>${dishId}</h4>
+                <p>${singleDishObj.description}</p>
+                <p class="dish_price">${(singleDishObj.price).toFixed(2)}€</p>
+            </div>
+            <button class="dish_button" onclick="addToBasket(${dishId})">
+                <img src="./assets/icons/add_60dp_FF8000.png" alt="add dish button">
+            </button>
+          </div>
+         `
 
-function saveDishesInLocalStorage(){
-  for (let index = 0; index < allDishes.length; index++) {
-    let dishJSON = JSON.stringify(allDishes[index])
-    localStorage.setItem(allDishes[index].name, dishJSON);
-  }
 }
