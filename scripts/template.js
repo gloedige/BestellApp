27c53bc7cbaf1d2eba374579fb189c8d5ee806cf
gloedige <gerd.loedige@posteo.de>
2025-09-05 15:08:55 +0200

    create template function renderSingleDishInBasket

--- scripts/template.js
+++ scripts/template.js
@@ -13,3 +13,27 @@ function renderSingleDishSection(dishId, singleDishObj){
          `
 
 }
+
+function renderSingleDishInBasket(singleDishObj){
+  return `
+          <div>
+            <p class="dishname_basket"></p>
+            <div class="container_price_and_quantity">
+                <button class="buton_basket">
+                    <img src="./assets/icons/remove_60dp_FF8000.png" alt="remove dish">
+                    <img src="./assets/icons/remove_circle_60dp_FF8000.png" alt="remove dish">
+                </button>
+                <p id="quantity_of_single_dish"></p>
+                <button class="buton_basket">
+                    <img src="./assets/icons/add_60dp_FF8000.png" alt="add dish">
+                    <img src="./assets/icons/add_circle_60dp_FF8000.png" alt="add dish">
+                </button>
+                <p id="total_price_of_single_dish"></p>
+                <button class="buton_basket">
+                    <img src="./assets/icons/delete_60dp_FF8000.png" alt="delete single dish">
+                </button>
+              </div>
+          </div>
+         `
+
+}
