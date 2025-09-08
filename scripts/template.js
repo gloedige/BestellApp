5b572af59f5fdb789234d1ded558215b1e27d6b4
gloedige <gerd.loedige@posteo.de>
2025-09-08 13:53:24 +0200

    create template for renderSingleDishInBasket

--- scripts/template.js
+++ scripts/template.js
@@ -17,19 +17,19 @@ function renderSingleDishSection(dishId, singleDishObj){
 function renderSingleDishInBasket(singleDishObj){
   return `
           <div>
-            <p class="dishname_basket"></p>
+            <p class="dishname_basket">${singleDishObj.name}</p>
             <div class="container_price_and_quantity">
-                <button class="buton_basket">
-                    <img src="./assets/icons/remove_60dp_FF8000.png" alt="remove dish">
-                    <img src="./assets/icons/remove_circle_60dp_FF8000.png" alt="remove dish">
+                <button class="button_basket remove_button">
+                    <img src="./assets/icons/remove_60dp_FF8000.png" alt="remove dish" class="remove_icon">
+                    <img src="./assets/icons/remove_circle_60dp_FF8000.png" alt="remove dish" class="remove_circle_icon d_none">
                 </button>
-                <p id="quantity_of_single_dish"></p>
-                <button class="buton_basket">
-                    <img src="./assets/icons/add_60dp_FF8000.png" alt="add dish">
-                    <img src="./assets/icons/add_circle_60dp_FF8000.png" alt="add dish">
+                <p id="quantity_of_single_dish">${singleDishObj.quantity}</p>
+                <button class="button_basket add_button">
+                    <img src="./assets/icons/add_60dp_FF8000.png" alt="add dish" id="add" class="add_icon">
+                    <img src="./assets/icons/add_circle_60dp_FF8000.png" alt="add dish" class="add_circle_icon d_none">
                 </button>
-                <p id="total_price_of_single_dish"></p>
-                <button class="buton_basket">
+                <p id="total_price_of_single_dish">${singleDishObj.price.toFixed(2)}</p>
+                <button class="button_basket" onclick="deleteDishFromBasket(${singleDishObj.name})">
                     <img src="./assets/icons/delete_60dp_FF8000.png" alt="delete single dish">
                 </button>
               </div>
