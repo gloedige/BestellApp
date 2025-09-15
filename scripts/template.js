8d2a317fa18ab0f16ff1a8b7c1a252485be08aa1
gloedige <gerd.loedige@posteo.de>
2025-09-15 15:12:52 +0200

    update classname of button in dish container

--- scripts/template.js
+++ scripts/template.js
@@ -1,12 +1,12 @@
 function renderSingleDishSection(dishId, singleDishObj){ // dishId is name of dish
   return `
-          <div class="single_dish" onmouseover="highlightDish('${dishId}')" onmouseout="highlightDish('${dishId}')" id="${dishId}">
+          <div class="single_dish" onmouseover="highlightDish('${dishId}')" onmouseout="highlightDish('${dishId}')" id="${dishId}" data-dish-id="${dishId}">
             <div class="dish_attributes">
                 <h4>${dishId}</h4>
                 <p>${singleDishObj.description}</p>
                 <p class="dish_price">${(singleDishObj.price).toFixed(2)}€</p>
             </div>
-            <button class="dish_button" onclick="addToBasket('${dishId}')">
+            <button class="add_dish_button" onclick="addToBasket('${dishId}')">
                 <img src="./assets/icons/add_60dp_FF8000.png" alt="add dish button">
             </button>
           </div>
